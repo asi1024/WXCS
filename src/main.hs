c1850585cc25129cd5e28366143f64bae89a7b27
asi1024 <asi1024@kmc.gr.jp>
2014-01-18 02:37:57 +0900

    create contest page

--- src/main.hs
+++ src/main.hs
@@ -7,13 +7,20 @@ import Data.Monoid (mconcat)
 import Data.Time
 import Control.Monad.IO.Class
 
+aojurl :: Int -> String
+aojurl n = "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=" ++ show n
+
 main :: IO ()
 main = scotty 16384 $ do
+  let user_id = "sss" :: String
+  current_time_ <- liftIO getCurrentTime
+  let current_time = show current_time_
   get "/" $ do
-    let user_id = "sss" :: String
-    current_time_ <- liftIO getCurrentTime
-    let current_time = show current_time_
     html $ renderHtml $ $(hamletFile "./template/index.hamlet") undefined
+  get "/contest/:word" $ do
+    contest_id <- param "word" :: ActionM String
+    let probA = aojurl 2272
+    html $ renderHtml $ $(hamletFile "./template/contest.hamlet") undefined
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
