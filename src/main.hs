53352d8220d109ce98491cbaad6e39c032d56310
asi1024 <asi1024@kmc.gr.jp>
2014-01-18 01:49:20 +0900

    create top page

--- src/main.hs
+++ src/main.hs
@@ -1,10 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, OverloadedStrings #-}
 import Web.Scotty
+import Text.Hamlet
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 import Data.Monoid (mconcat)
+import Data.Time
+import Control.Monad.IO.Class
 
+main :: IO ()
 main = scotty 16384 $ do
-  get "/index" $ file "/home/asi1024/HTML/WXCS/index.html"
+  get "/" $ do
+    let user_id = "sss" :: String
+    current_time_ <- liftIO getCurrentTime
+    let current_time = show current_time_
+    html $ renderHtml $ $(hamletFile "./template/index.hamlet") undefined
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
