025319de9db09c90f560f32bbb2e6e51ccee66d7
asi1024 <asi1024@kmc.gr.jp>
2014-01-22 01:07:02 +0900

    variablize contest.hamlet

--- src/main.hs
+++ src/main.hs
@@ -20,11 +20,18 @@ import Text.Hamlet
 
 import Web.Scotty
 
-aojurl :: Int -> String
-aojurl n = "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=" ++ show n
+aojurl :: String -> String
+aojurl n = "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=" ++ n
 
 contest_list :: [(String, String, String, String, String)]
 contest_list = [("1", "ICPC Study Session Part. 1", "AOJ", "2014-01-01 00:00:00", "2014-01-01 00:01:00"), ("2", "ICPC Study Session Part. 2", "AOJ", "2014-01-02 00:00:00", "2014-01-02 00:01:00")]
+
+problem_set :: [(String, String, String, String, String)]
+problem_set = [("stAC", "2272", aojurl "2272", "B", "Accepted"),
+               ("stAC", "2274", aojurl "2274", "D", "Accepted"),
+               ("stWA", "2276", aojurl "2276", "F", "Wrong Answer"),
+               ("odd",  "2278", aojurl "2278", "H", ""),
+               ("even", "2280", aojurl "2280", "J", "")]
 
 main :: IO ()
 main = scotty 16384 $ do
@@ -44,8 +51,10 @@ main = scotty 16384 $ do
     timezone <- liftIO getCurrentTimeZone
     current_time_ <- liftIO getCurrentTime
     let current_time = show $ utcToLocalTime timezone current_time_
-    contest_id <- param "word" :: ActionM String
-    let probA = aojurl 2272
+    let contest_name = "ICPC Study Session Part. 1" :: String
+    let contest_type = "AOJ" :: String
+    let start_time = "start" :: String
+    let end_time = "end" :: String
     html $ renderHtml $ $(hamletFile "./template/contest.hamlet") undefined
 
   post "/submit" $ do
