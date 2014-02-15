eb3cc4e27cb06b2b9fcc571257f896a6c43829be
Yuichiro Hanada <peanut2626@gmail.com>
2014-02-15 12:39:03 +0900

    Move findSubmit and updateSubmit to Model.

--- src/Submit.hs
+++ src/Submit.hs
@@ -7,7 +7,7 @@ module Submit (
 import Control.Concurrent
 import Control.Monad (liftM)
 
-import Database.Persist ((==.), Filter)
+import Database.Persist ((==.))
 import qualified Database.Persist.Sqlite as Sq
 
 import Config
@@ -15,28 +15,19 @@ import Model
 import ModelTypes
 import qualified OnlineJudge as OJ
 
-findSubmit :: Configuration -> [Filter Submit] -> IO (Maybe (Sq.Entity Submit))
-findSubmit conf filt = Sq.runSqlite (db conf) $ Sq.selectFirst filt []
-
 findPendingSubmit :: Configuration -> IO (Maybe Submit)
 findPendingSubmit conf = do
-  submit' <- findSubmit conf [SubmitJudge ==. Pending]
+  submit' <- findSubmit (db conf) [SubmitJudge ==. Pending]
   return $ liftM Sq.entityVal $ submit'
-
-mkSubmission :: Submit
-                -> JudgeStatus
-                -> String -- time
-                -> String -- memory
-                -> Submit
-mkSubmission s j t m = s { submitJudge = j, submitTime = t, submitMemory = m }
 
 getAndUpdateWithRunId :: Configuration -> Submit -> Int -> IO ()
 getAndUpdateWithRunId conf submit rid = do
   res <- OJ.fetchByRunId conf (submitJudgeType submit) rid
   case res of
-    Nothing -> updateSubmit conf $ submit { submitJudge = SubmissionError }
+    Nothing -> updateSubmit (db conf) $ submit { submitJudge = SubmissionError }
     Just (judge, time, mem) -> do
-      updateSubmit conf $ mkSubmission submit judge time mem
+      updateSubmit (db conf) $
+        submit { submitJudge = judge, submitTime = time, submitMemory = mem }
 
 getResultAndUpdate :: Configuration -> Submit -> Int -> IO ()
 getResultAndUpdate conf submit latest_run_id = loop (0 :: Int)
@@ -51,23 +42,15 @@ getResultAndUpdate conf submit latest_run_id = loop (0 :: Int)
       else
         updateSubmit conf (submit { submitJudge = SubmissionError } )
 
-updateSubmit :: Configuration -> Submit -> IO ()
-updateSubmit conf s = do
-  submit <- findSubmit conf
-            [SubmitSubmitTime ==. (submitSubmitTime s), SubmitUserId ==. (submitUserId s)]
-  case liftM Sq.entityKey $ submit of
-    Nothing -> return ()
-    Just submit_id -> Sq.runSqlite (db conf) $ Sq.replace submit_id s
-
 submitAndUpdate :: Configuration -> Submit -> IO ()
 submitAndUpdate conf s = do
   last_run_id <- OJ.getLatestRunId conf (submitJudgeType s)
-  updateSubmit conf (s { submitJudge = Running })
+  updateSubmit (db conf) (s { submitJudge = Running })
   success <- OJ.submit conf (submitJudgeType s) (submitProblemId s)
              (submitLang s) (submitCode s)
   if success
     then getResultAndUpdate conf s last_run_id
-    else updateSubmit conf $ s { submitJudge = SubmissionError }
+    else updateSubmit (db conf) $ s { submitJudge = SubmissionError }
 
 crawler :: Configuration -> IO ()
 crawler conf = do
